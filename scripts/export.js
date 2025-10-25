8f8925fe3596a539d2056cdfcdfc0b2ebb868d7f
skoniks <skolya16@ya.ru>
2025-10-25 11:21:25 +0300

    Update galaxymap

--- scripts/export.js
+++ scripts/export.js
@@ -2,12 +2,14 @@ const { parse, stringify } = require('csv/sync');
 const { readFileSync, writeFileSync } = require('node:fs');
 
 async function bootstrap() {
+  const filter = ['OBSOLETE', 'PREVIOUS'];
   const file1 = readFileSync('./language.csv', 'utf8');
   const file2 = readFileSync('./original/original.csv', 'utf8');
   const custom = parse(file1, { columns: true, bom: true });
   const original = parse(file2, { columns: true, bom: true });
   //
   const data = custom.filter((i) => {
+    if (filter.includes(i.state)) return false;
     const j = original.find((j) => j.key === i.key);
     return j && j.translation !== i.translation;
   });
